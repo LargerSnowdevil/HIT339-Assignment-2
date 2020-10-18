4b9a96ea0b70ab841aaa7c18612ddf7c42d47b74
Daniel Wilson <drac1223@hotmail.com>
2020-10-18 09:46:17 +0930

    Set up database

---
+++ AnyoneForTennis/AnyoneForTennis/Data/AppDBContext.cs
@@ -0,0 +1,40 @@
+﻿using AnyoneForTennis.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnyoneForTennis.Data
+{
+    public class AppDBContext : IdentityDbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Coach> Coaches { get; set; }
+
+        public DbSet<Member> Members { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<EventMember>()
+                .HasKey(em => new { em.EventId, em.MemberId });
+            modelBuilder.Entity<EventMember>()
+                .HasOne(em => em.Event)
+                .WithMany(e => e.EventMembers)
+                .HasForeignKey(em => em.EventId);
+            modelBuilder.Entity<EventMember>()
+                .HasOne(em => em.Member)
+                .WithMany(m => m.EventMembers)
+                .HasForeignKey(em => em.MemberId);
+        }
+    }
+}
