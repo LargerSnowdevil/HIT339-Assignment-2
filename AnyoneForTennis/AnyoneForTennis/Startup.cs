ca7bd720798f519a6629b2fe5a1b0b3ee11c6bc9
Daniel Wilson <drac1223@hotmail.com>
2020-10-18 09:59:30 +0930

    Scaffold CRUD for database tables

--- AnyoneForTennis/AnyoneForTennis/Startup.cs
+++ AnyoneForTennis/AnyoneForTennis/Startup.cs
@@ -6,6 +6,7 @@ using AnyoneForTennis.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
