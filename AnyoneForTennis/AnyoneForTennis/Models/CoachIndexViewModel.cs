307e658434fea1eb42ae0299faa4fe2aa54be03b
Daniel Wilson <drac1223@hotmail.com>
2020-10-19 12:01:48 +0930

    Final Clean-up

--- AnyoneForTennis/AnyoneForTennis/Models/CoachIndexViewModel.cs
+++ AnyoneForTennis/AnyoneForTennis/Models/CoachIndexViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace AnyoneForTennis.Models
 {
     public class CoachIndexViewModel
     {
+        [DisplayName("Coach Id")]
         public int CoachId { get; set; }
 
         public string Name { get; set; }
--- AnyoneForTennis/AnyoneForTennis/Models/EventEnrollViewModel.cs
+++ AnyoneForTennis/AnyoneForTennis/Models/EventEnrollViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace AnyoneForTennis.Models
 
         public string Location { get; set; }
 
+        [DisplayName("Running Coach")]
         public string RunningCoach { get; set; }
     }
 }
